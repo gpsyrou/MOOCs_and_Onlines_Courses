a664bd268a8415056c14697841d31748f40eeb71
gpsyrou <gspyrou@umich.edu>
2020-05-02 10:49:09 +0100

    update input functionality for caesar

--- CS50_Harvard_Intro_to_Computer_Science/Week2/caesar.c
+++ CS50_Harvard_Intro_to_Computer_Science/Week2/caesar.c
@@ -9,7 +9,19 @@ int main (int argc, string argv[])
     // Handle the case where the user inputs less or more than 1 argument
     if (argc != 2)
     {
-        printf("Incorrect number of command line arguments. Please input a single argument.");
+        printf("Incorrect number of command line arguments. Please input a single argument.\n");
         return 1;
     }
+
+    printf("The argument was %s\n", argv[1]);
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (!(isdigit(argv[1][i])))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
+    }
+
+    return 0;
 }
