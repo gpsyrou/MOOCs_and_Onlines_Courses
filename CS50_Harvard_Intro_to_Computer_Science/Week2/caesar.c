5dd30eb94c1121c48f2a70db88593e8a39a43b7e
gpsyrou <gspyrou@umich.edu>
2020-05-02 10:12:14 +0100

    create caesar file for task 2

---
+++ CS50_Harvard_Intro_to_Computer_Science/Week2/caesar.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# include <cs50.h>
+# include <string.h>
+# include <ctype.h>
+# include <math.h>
+
+int main (int argc, string argv[])
+{
+    // Handle the case where the user inputs less or more than 1 argument
+    if (argc != 2)
+    {
+        printf("Incorrect number of command line arguments. Please input a single argument.");
+        return 1;
+    }
+}
