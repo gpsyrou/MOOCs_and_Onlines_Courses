4b3f680ad641ed150334c4b24eb01bcf2460eb03
gpsyrou <gspyrou@umich.edu>
2020-05-16 10:52:23 +0100

    fix bug with char and int

--- CS50_Harvard_Intro_to_Computer_Science/Week2/caesar.c
+++ CS50_Harvard_Intro_to_Computer_Science/Week2/caesar.c
@@ -5,45 +5,8 @@
 # include <math.h>
 # include <stdlib.h>
 
-// Function that performs adds the key to the plain text
-// and returns the ciphertext
-void encrypt_plaintext(int val)
-{
-    string p = get_string("plaintext:");
 
-    int char_plus_key;
-    char output[strlen(p)]; //This will hold the final output
-
-    for (int i = 0; i < strlen(p); i++)
-    {
-
-        if (isalpha(p[i]))
-        {
-             char_plus_key = p[i] + val;
-
-            // Add condition to do a "cycle" if we violate the ASCII boundaries
-            if (char_plus_key > 90 && char_plus_key < 97)
-            {
-                char_plus_key = 64 + (char_plus_key - 90);
-            }
-            else if (char_plus_key > 122)
-            {
-                char_plus_key = 96 + (char_plus_key - 122);
-            }
-
-        }
-        else
-        {
-            char_plus_key = p[i];
-        }
-        output[i] = char_plus_key;
-    }
-
-    printf("ciphertext: %s", output);
-}
-
-
-int main (int argc, string argv[])
+int main(int argc, string argv[])
 {
     // Handle the case where the user inputs less or more than 1 argument
     if (argc != 2)
@@ -62,10 +25,44 @@ int main (int argc, string argv[])
     }
 
     // Convert the command line argument from string to int
-    int input_value = atoi(argv[1]);
+    int val = atoi(argv[1]);
 
     // Prompt user for a plaintext
-    encrypt_plaintext(input_value);
+    string p = get_string("plaintext:");
+
+    int char_plus_key;
+    char output[strlen(p)]; //This will hold the final output
+
+    for (int i = 0; i < strlen(p); i++)
+    {
+        if (p[i] != '\n')
+        {
+            if (isalpha(p[i]))
+            {
+                char_plus_key = p[i] + val;
+
+                // Add condition to do a "cycle" if we violate the ASCII boundaries
+                if (char_plus_key > 90 && char_plus_key < 97)
+                {
+                    char_plus_key = 64 + (char_plus_key - 90);
+                }
+                else if (char_plus_key > 122)
+                {
+                    char_plus_key = 96 + (char_plus_key - 122);
+                }
+
+            }
+            else
+            {
+                char_plus_key = p[i];
+            }
+            // Convert the number to a characterand append it to the string
+            sprintf(&output[i], "%c", char_plus_key);;
+        }
+    }
+
+    // Print the final output for the ciphertext
+    printf("ciphertext: %s\n", output);
 
     return 0;
 }
